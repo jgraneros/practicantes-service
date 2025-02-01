42ba78059b07819cb90684514c3cd91194408a46
juanjo <jgraneros47@gmail.com>
2025-02-01 17:08:54 -0300

    Cambios

--- src/main/webapp/practicantes-frontend/src/components/PracticantesSearch.vue
+++ src/main/webapp/practicantes-frontend/src/components/PracticantesSearch.vue
@@ -3,22 +3,17 @@
         <h1>Buscar por DNI</h1>
         <form @submit.prevent="searchPracticante">
             <label for="dni">DNI: </label>
-            <input type="text"
-             id="dni"
-             v-model="dni"
-             placeholder="Ingrese el dni"
-             required
-            />
+            <input type="text" id="dni" v-model="dni" placeholder="Ingrese el dni" required />
 
             <button type="submit">Buscar</button>
         </form>
 
         <div v-if="loading">cargando...</div>
-        <div v-if="error" class="error">{{error}}</div>
+        <div v-if="error" class="error">{{ error }}</div>
 
         <div v-if="practicante">
             <h2>Resultado</h2>
-            <table>
+            <table class="content-table">
                 <thead>
                     <tr>
                         <th>Nombre</th>
@@ -39,7 +34,7 @@
 
             <dialog v-if="showCuotasModal" open class="modal">
                 <h3>Cuotas de {{ practicante?.nombre }} {{ practicante?.apellido }}</h3>
-                <table>
+                <table class="content-table">
                     <thead>
                         <tr>
                             <th>Fecha</th>
@@ -64,115 +59,87 @@
 
 <script setup lang="ts">
 
-    import { ref } from 'vue';
-    import axios from 'axios';
+import { ref } from 'vue';
+import axios from 'axios';
 
-    interface Cuota {
-        fecha: String;
-        estado: String;
-        mes: String;
+interface Cuota {
+    fecha: String;
+    estado: String;
+    mes: String;
+}
+
+interface Practicante {
+    nombre: String;
+    apellido: String;
+    cuotas: Cuota[];
+}
+
+
+
+const token = 'eyJhbGciOiJSUzI1NiIsInR5cCIgOiAiSldUIiwia2lkIiA6ICIyNk5maTRUZGFzb2pmTDRIOFJDMFEwd04wWnNuXzFiR2x5Qkd6NkY5Y0JZIn0.eyJleHAiOjE3Mzg0NDAyNDEsImlhdCI6MTczODQzOTY0MSwianRpIjoiMjRkMjRjODYtMTFiYS00ZWQ2LWIzY2MtZThkOGRmNDU4ZjU2IiwiaXNzIjoiaHR0cDovL2xvY2FsaG9zdDozMjc3MC9yZWFsbXMvcXVhcmt1cyIsInR5cCI6IkJlYXJlciIsImF6cCI6InF1YXJrdXMtYXBwIiwic2lkIjoiN2U4MjAyM2QtYThiYi00ODFiLTlkMzktMDhjM2FmNzYyNDZhIiwic2NvcGUiOiJtaWNyb3Byb2ZpbGUtand0IiwidXBuIjoianVhbiIsImdyb3VwcyI6WyJpbnN0cnVjdG9yIiwib2ZmbGluZV9hY2Nlc3MiLCJkZWZhdWx0LXJvbGVzLXF1YXJrdXMiLCJ1bWFfYXV0aG9yaXphdGlvbiJdfQ.TonKJPkAvHMkEl_cgKiHw9iWpHXA4Sxm7KZrX53_Mb8msrgLtaJHJDsN9n2AfwtnYWOeSZiI_JLYai1rXW3131uqrDlD_QSEBMBpK_8g_RQWnj9ViLW43gKevhdD4qkrxmf5gKssU7d2n0xSteS3Xw_J0QgHp-8PfBfzcs31I7nGcRVl048duVa6NVXLsdnZaMEUkZ0vgWx7e3819XjW_iP01pDU4ax9i-5lHx0huMgdt_Yezkas_imSumIc0k1zu5XYlSJxYx5la_8Lxp-G771DH9dZgncR9IoTGnrscWM7IejtM4qvAcqe0fOsgX07iW6IjMD_dCsGZcXRuoP82A';
+
+
+const dni = ref<string>('')
+const practicante = ref<Practicante | null>(null);
+const loading = ref<boolean>(false);
+const error = ref<string>('');
+const showCuotasModal = ref<boolean>(false);
+
+const searchPracticante = async (): Promise<void> => {
+
+    if (!dni.value) {
+        error.value = "Por favor ingrese un dni valido"
+        return;
     }
 
-    interface Practicante {
-        nombre: String;
-        apellido: String;
-        cuotas: Cuota[];
-    }
+    loading.value = true;
+    error.value = '';
+    practicante.value = null;
 
-
-
-    const token = 'eyJhbGciOiJSUzI1NiIsInR5cCIgOiAiSldUIiwia2lkIiA6ICJZdUdfdXpIY0tnV1FCb1pZV1RPU0RuVTZLeTZ0QmlSNlRmdVhJQi1QQ0tVIn0.eyJleHAiOjE3MzgyNTg2OTgsImlhdCI6MTczODI1ODA5OCwianRpIjoiZTc3ODQ4YWQtMzJkOC00YzkxLWI1OTAtZGQ2ZTkwMGI4N2RmIiwiaXNzIjoiaHR0cDovL2xvY2FsaG9zdDozMjc3MC9yZWFsbXMvcXVhcmt1cyIsInR5cCI6IkJlYXJlciIsImF6cCI6InF1YXJrdXMtYXBwIiwic2lkIjoiZmFkNjUwZjMtMDBkZS00ZmYwLWIwMDItMWVhMzU0NjJkOTU3Iiwic2NvcGUiOiJtaWNyb3Byb2ZpbGUtand0IiwidXBuIjoianVhbiIsImdyb3VwcyI6WyJpbnN0cnVjdG9yIiwib2ZmbGluZV9hY2Nlc3MiLCJkZWZhdWx0LXJvbGVzLXF1YXJrdXMiLCJ1bWFfYXV0aG9yaXphdGlvbiJdfQ.wX1kr-bn1y4piSRBhwqZDJJP27n2hzjNTAMnraWc6AggVOtzZXgYI5-yGnLZc_E1wdvsPA5voVu6dONHmGCLjhLP1JZ49EnXGW6bBQg-cdbOUCXCUtVUMQFF2Zyr2hN4chQk-PIvkGxo2Sil2Wr5aP9-SaFbKWbyqLuMVjpQvwUsq5WH_9XZfFAXnIx-8Lbu8QElaOy1RD40f3HA4xNTClBahyPEDl39LouIMSah_Zl9gjOBpeWBQdlDqAti-r0DtgvgovNdzutvroEXluyQlSHnoh3hzQ7ITlcKrZ-qQSRyGHHKroDVDo93WUaUvsxsGz4h6CjrEIXxJVqkA87nbw';
-
-
-    const dni = ref<string>('')
-    const practicante = ref<Practicante  | null>(null);
-    const loading = ref<boolean>(false);
-    const error = ref<string>('');
-    const showCuotasModal = ref<boolean>(false);
-
-    const searchPracticante = async (): Promise<void> => {
-
-        if (!dni.value) {
-            error.value = "Por favor ingrese un dni valido"
-            return;
-        }
-
-        loading.value = true;
-        error.value = '';
-        practicante.value = null;
-
-        try {
-            const response = await axios.get('http://localhost:8081/practicantes/v1', {
-                params: {dni: dni.value},
-                headers: {
+    try {
+        const response = await axios.get('http://localhost:8081/practicantes/v1', {
+            params: { dni: dni.value },
+            headers: {
                 Authorization: `Bearer ${token}`, // Envía el token en el header
-                },
-            });
+            },
+        });
 
-            practicante.value = response.data;
-        } catch (err) {
-            error.value = "Error"
-            console.log(err)
-        } finally {
-            loading.value = false;
-        }
+        practicante.value = response.data;
+    } catch (err) {
+        error.value = "Error"
+        console.log(err)
+    } finally {
+        loading.value = false;
     }
+}
 
 </script>
 
 <style scoped>
-    .error {
+.error {
     color: red;
-    }
+}
 
+.content-table {
+    border-collapse: collapse;
+    margin: 25px 0;
+    font-size: 0.9em;
+    min-width: 400px;
+}
 
-    table {
-        width: 100%;
-        border-collapse: collapse;
-        margin-top: 1rem;
-    }
+.content-table thead tr {
+    background-color: #009879;
+    color: #ffffff;
+    text-align: left;
+    font-weight: bold;
+}
 
-    th, td {
-        border: 1px solid #ddd;
-        padding: 8px;
-        text-align: left;
-      }
+.content-table th,
+.content-table td {
+    padding: 12px 15px;
+}
 
-      th {
-        background-color: #f4f4f4;
-      }
-
-      button {
-        cursor: pointer;
-      }
-
-      .modal-background {
-        position: fixed;
-        top: 0;
-        left: 0;
-        width: 100%;
-        height: 100%;
-        background-color: rgba(0, 0, 0, 0.5); /* Fondo semitransparente */
-        z-index: 1000; /* Asegura que esté por encima de todo */
-      }
-      
-      .modal {
-        position: fixed;
-        top: 50%;
-        left: 50%;
-        transform: translate(-50%, -50%); /* Centra el modal */
-        background-color: white;
-        padding: 1rem;
-        border-radius: 8px;
-        box-shadow: 0 2px 10px rgba(0, 0, 0, 0.1);
-        z-index: 1001; /* Asegura que esté por encima del fondo */
-      }
-      
-      .modal h3 {
-        margin-top: 0;
-      }
-      
-      .modal button {
-        margin-top: 1rem;
-      }
+.content-table tbody tr {
+    border-bottom: 1px solid #dddddd;
+}
 </style>
